c6b7e712df56d9ff90079349491366af9bc447fa
ishansaini194 <ishan.saini880@gmail.com>
2026-05-06 12:07:15 +0530

    notice models

---
+++ api/models/notice.go
@@ -0,0 +1,21 @@
+package models
+
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
+type Notice struct {
+	ID         uint `gorm:"primaryKey" json:"id"`
+	SchoolID   uint `gorm:"not null;index:idx_notices_school_created,priority:1" json:"school_id"`
+	PostedByID uint `gorm:"not null" json:"posted_by_id"`
+
+	Title           string `gorm:"size:200;not null" json:"title"`
+	Body            string `gorm:"type:text;not null" json:"body"`
+	TargetAllSchool bool   `gorm:"not null;default:false" json:"target_all_school"`
+
+	CreatedAt time.Time      `gorm:"index:idx_notices_school_created,priority:2,sort:desc" json:"created_at"`
+	UpdatedAt time.Time      `json:"updated_at"`
+	DeletedAt gorm.DeletedAt `gorm:"index" json:"-"`
+}
